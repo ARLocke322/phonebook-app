29ea12ccd4992b09db97c40eb8e32a0b6fcf8eeb
ARLocke322 <alocke322@gmail.com>
2025-09-28 17:52:26 +0100

    fixed index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen("config.PORT", () => {
-  logger.info(`Server running on port ${config.PORT}`)
+const PORT = config.PORT || 3000
+app.listen(PORT, '0.0.0.0', () => {
+  logger.info(`Server running on port ${PORT}`)
 })
